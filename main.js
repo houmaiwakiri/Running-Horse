e3415e7a1d0f49000659211a2ca90bf20d9bbff9
houmaiwakiri <mahomahoma888@gmail.com>
2021-09-25 20:09:35 +0900

    完成

---
+++ main.js
@@ -0,0 +1,9 @@
+let rati = document.querySelector(".rati");
+let car= document.querySelector(".car")
+
+addEventListener("keypress", function(x){
+    if(x.which == 13){
+        rati.classList.toggle("moving");
+        car.classList.toggle("suspention");
+    }
+})
